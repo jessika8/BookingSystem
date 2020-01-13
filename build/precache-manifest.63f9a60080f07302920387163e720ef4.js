65ab3647f40cccfd3b28d556620dad60ff1a1cf7
Maarja Roots <codenation@Codes-MacBook-Pro.local>
2020-01-13 15:03:10 +0000

    changes in about page

---
+++ build/precache-manifest.63f9a60080f07302920387163e720ef4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71da202c16cd53fe7929df544243f79",
+    "url": "/BookingSystem/index.html"
+  },
+  {
+    "revision": "1432882c5e5fc01c7523",
+    "url": "/BookingSystem/static/css/2.8f9040a3.chunk.css"
+  },
+  {
+    "revision": "507c4eb5fd9aa00bd15f",
+    "url": "/BookingSystem/static/css/main.21ccca88.chunk.css"
+  },
+  {
+    "revision": "1432882c5e5fc01c7523",
+    "url": "/BookingSystem/static/js/2.a90a14de.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/BookingSystem/static/js/2.a90a14de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "507c4eb5fd9aa00bd15f",
+    "url": "/BookingSystem/static/js/main.ac2507a0.chunk.js"
+  },
+  {
+    "revision": "fe131af0219007daff9e",
+    "url": "/BookingSystem/static/js/runtime-main.b854fa20.js"
+  },
+  {
+    "revision": "48e4dec3f46faacb4cd0e89f125812ab",
+    "url": "/BookingSystem/static/media/alone_at_sea.48e4dec3.png"
+  },
+  {
+    "revision": "aafc2e2f2d85cfbf29366ed30eccb7ec",
+    "url": "/BookingSystem/static/media/sunbed.aafc2e2f.jpg"
+  },
+  {
+    "revision": "7866cc7bd7411476533189b04e59815a",
+    "url": "/BookingSystem/static/media/unicorn.7866cc7b.jpg"
+  }
+]);
